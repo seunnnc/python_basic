92d9c50a3c51289bef0fef11c471f48e47cd8025
seunnnc <seunnnc@users.noreply.github.com>
2020-11-23 17:42:32 +0900

    오프라인 스터디 랜덤 날짜 구하기

--- q2.py
+++ q2.py
@@ -2,6 +2,11 @@
 # 4일부터 28일 이내
 
 from random import *
+'''
 studyDate = int(random() * 24) + 4
 
+print('오프라인 스터디 모임 날짜는 매월', studyDate, '일로 선정되었습니다.')
+'''
+
+studyDate = randint(4, 28)
 print('오프라인 스터디 모임 날짜는 매월', studyDate, '일로 선정되었습니다.')
